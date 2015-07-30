193505b4a82414c9b16851d3926d431d12691e5d
jnhimanshu <jnhimanshu1000@gmail.com>
2015-07-30 22:52:20 +0530

    UI changes for Home, Contact and AboutUs page

---
+++ Web/app/app.js
@@ -0,0 +1,9 @@
+﻿angular.module('IGI', ['ngRoute', 'Enterprise.Controller'])
+
+.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/login', { templateUrl: 'partials/Login.html', Controller: 'loginController' });
+    $routeProvider.when('/Home', { templateUrl: 'partials/Home.html', Controller: 'homeController' });
+    $routeProvider.when('/ContactUs', { templateUrl: 'partials/Contact.html', Controller: 'contactController' });
+    $routeProvider.when('/AboutUs', { templateUrl: 'partials/AboutUs.html', Controller: 'aboutusController' });
+    $routeProvider.otherwise({ redirectTo: '/home' });
+}]);
---
+++ Web/app/controller/Controller.js
@@ -0,0 +1,21 @@
+﻿angular.module('Enterprise.Controller', [])
+
+.controller('IndexController', ['$scope', function ($scope) {
+    
+}])
+
+.controller('homeController', ['$scope', function ($scope) {
+}])
+
+.controller('contactController', ['$scope', function ($scope) {
+    $("#home").removeAttr("style");
+}])
+
+    .controller('aboutusController', ['$scope', function ($scope) {
+        $("#home").removeAttr("style");
+    }])
+
+
+.controller('loginController', ['$scope', function ($scope) {
+    
+}]);
