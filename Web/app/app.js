9f8eb8bb2cc616700bf7246ef8cf3de8e8e15a19
jnhimanshu <jnhimanshu1000@gmail.com>
2015-08-02 13:25:35 +0530

    Added few more pages

--- Web/app/app.js
+++ Web/app/app.js
@@ -1,17 +1,18 @@
-﻿angular.module('IGI', ['ngRoute', 'Enterprise.Controller', 'Enterprise.Services'])
+﻿
+angular.module('IGI', ['ngRoute', 'Enterprise.Controller', 'Enterprise.Services'])
 
 .run(['$rootScope', function ($rootScope) {
     localStorage['webApiUrl'] = window.location.protocol + "//" + window.location.hostname + "/api/";
 }])
 
 .config(['$routeProvider', function ($routeProvider) {
-    $routeProvider.when('/login', { templateUrl: 'partials/Login.html', Controller: 'loginController' });
-    $routeProvider.when('/Home', { templateUrl: 'partials/Home.html', Controller: 'homeController' });
-    $routeProvider.when('/ContactUs', { templateUrl: 'partials/Contact.html', Controller: 'contactController' });
-    $routeProvider.when('/AboutUs', { templateUrl: 'partials/AboutUs.html', Controller: 'aboutusController' });
-    $routeProvider.when('/Courses', { templateUrl: 'partials/Courses.html', Controller: 'courseController' });
-    $routeProvider.when('/ToolKit', { templateUrl: 'partials/Courses.html', Controller: 'toolkitController' });
-    $routeProvider.when('/Login', { templateUrl: 'partials/Login.html', Controller: 'LoginController' });
-    $routeProvider.when('/Register', { templateUrl: 'partials/Login.html', Controller: 'LoginController' });
+    $routeProvider.when('/login', { templateUrl: 'partials/Login.html', controller: 'loginController' });
+    $routeProvider.when('/Home', { templateUrl: 'partials/Home.html', controller: 'homeController' });
+    $routeProvider.when('/ContactUs', { templateUrl: 'partials/Contact.html', controller: 'contactController' });
+    $routeProvider.when('/AboutUs', { templateUrl: 'partials/AboutUs.html', controller: 'aboutusController' });
+    $routeProvider.when('/Courses', { templateUrl: 'partials/Courses.html', controller: 'courseController' });
+    $routeProvider.when('/ToolKit', { templateUrl: 'partials/Courses.html', controller: 'toolkitController' });
+    $routeProvider.when('/Login', { templateUrl: 'partials/Login.html', controller: 'LoginController' });
+    $routeProvider.when('/Register', { templateUrl: 'partials/Login.html', controller: 'LoginController' });
     $routeProvider.otherwise({ redirectTo: '/Home' });
 }]);
--- Web/app/controller/Controller.js
+++ Web/app/controller/Controller.js
@@ -9,11 +9,7 @@
 
 .controller('LoginController', ['$scope', function ($scope) {
     $("#home").removeAttr("style");
-
-    $scope.dateOptions = {
-        'year-format': "'yy'",
-        'starting-day': 1
-    };
+    $scope.emailPattern = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/i;
 }])
 
 .controller('contactController', ['$scope', function ($scope) {
@@ -24,7 +20,8 @@
     $("#home").removeAttr("style");
 }])
 
-.controller('courseController', ['$scope', 'EnterpriseService', function ($scope, EnterpriseService) {
+
+.controller('courseController', ['$scope', 'EnterpriseService', '$filter', function ($scope, EnterpriseService, $filter) {
     $("#home").removeAttr("style");
     $scope.CourseList = {
         courseName: '',
@@ -33,9 +30,133 @@
         imageURL: '',
         description: ''
     };
+    $scope.showAddCourse = true;
     EnterpriseService.GetAllCourses().success(function (data) {
         $scope.CourseList = data;
     }).error(function () { });
+
+    //  Checking extension of the uploaded file
+    $scope.checkFileExtension = function (fileAttached) {
+        for (var id = 0; id < fileAttached.length; id++) {
+            var filename = fileAttached[id].name;
+            var ext;
+
+            var dot_pos = filename.lastIndexOf(".");
+            if (dot_pos == -1)
+                return "";
+            ext = filename.substr(dot_pos + 1).toLowerCase();
+
+            if (ext === "exe" || ext === "dll" || ext == "js" || ext == "htm" || ext == "html") {
+                $('#txtFileUpload').val("");
+                $scope.showAttachmentAlertMessage("Invalid file type.", true);
+            } else {
+                continue;
+            }
+        }
+    }
+
+    $scope.showAttachmentAlertMessage = function (alertMessage, isShow) {
+        if (isShow) {
+            $("#attachmentLabel").show();
+            $("#errorAttachmentText").html(alertMessage);
+
+            $timeout(function () {
+                $("#attachmentLabel").hide();
+                $("#errorAttachmentText").html("");
+            }, 15000);
+        }
+    }
+
+    $scope.fileAttachments = [];
+
+    $scope.setFiles = function (element) {
+        // Only atmost 2 files can be selected
+        if (element.files.length <= 2) {
+            var totalSizeLimitInBytes = 1024 * 1024 * parseInt(10);
+            var usedSizeLimit = 0
+            var attachedFiles = element.files;
+            $scope.checkFileExtension(attachedFiles);
+            // Filtering those attachments that have IsDeleted=false.
+            var unDeletedAttachments = $filter('filter')($scope.fileAttachments, { IsDeleted: false });
+
+            var remainingSizeLimit = 0;
+            var limitedNoOfFiles = 3;
+
+            // Checking number of file attached
+            // Checking size limit for the attachments
+            // Saving attachment to the DB
+            if ((attachedFiles.length + unDeletedAttachments.length) <= limitedNoOfFiles) {
+                for (var ctr = 0; ctr < attachedFiles.length; ctr++) {
+                    if (attachedFiles[ctr].size) {
+                        usedSizeLimit = usedSizeLimit + parseInt(attachedFiles[ctr].size)
+                    }
+                }
+                for (var ctr = 0; ctr < unDeletedAttachments.length; ctr++) {
+                    if (unDeletedAttachments[ctr].FileSize) {
+                        usedSizeLimit = usedSizeLimit + parseInt(unDeletedAttachments[ctr].FileSize)
+                    }
+                }
+                var remainingSizeLimit = totalSizeLimitInBytes - usedSizeLimit;
+
+                // If the size of the attachments size are lesser than the required size
+                if (remainingSizeLimit <= 0) {
+                    $('#txtFileUpload').val("");
+                    $scope.showAttachmentAlertMessage("Size of attachment exceeds the maximum size limit.", true);
+                } else {
+
+                    attachedFiles = $.merge(attachedFiles, unDeletedAttachments);
+
+                    // pushing checked attachments to the fileAttachments array.
+                    if (attachedFiles.length <= limitedNoOfFiles) {
+                        var fd = new FormData();
+                        for (var i in attachedFiles) {
+                            fd.append("uploadedFile", attachedFiles[i])
+                        }
+
+                        //createIntershipRequest.uploadAttachment(fd)
+                        //    .success(function (data) {
+                        //        if (data) {
+                        //            if (data.attachedFiles && data.attachedFiles.length) {
+                        //                for (var i = 0, iLen = data.attachedFiles.length; i < iLen; i++) {
+                        //                    $scope.InternshipDetails.fileAttachments.push(data.attachedFiles[i]);
+                        //                }
+                        //            }
+                        //            if (data.erroredAttachments) {
+                        //                $scope.showAttachmentAlertMessage(data.erroredAttachments, true);
+                        //            }
+                        //        } else {
+                        //            $scope.showAttachmentAlertMessage("File cannot be uploaded due to some error. Please try again.", true);
+                        //        }
+                        //    }).error(function (data, status, headers, config) {
+                        //        $scope.showAttachmentAlertMessage(data.Message, true);
+                        //        log.error("Error ~ " + status + config.toString() + data.toString() + headers.toString());
+                        //    });
+                        $scope.fileAttachments.push({ OriginalName: element.files[0].name, IsDeleted: false });
+                        if (!$scope.$$phase) {
+                            $scope.$apply();
+                        }
+
+                    }
+                    else {
+                        $('#txtFileUpload').val("");
+                        $scope.showAttachmentAlertMessage("Number of attachments reached the maximum limit.", true);
+                    }
+                }
+            }
+            else {
+                $('#txtFileUpload').val("");
+                $scope.showAttachmentAlertMessage("Number of attachments reached the maximum limit.", true);
+            }
+        }
+        else {
+            $scope.showAttachmentAlertMessage("Number of attachments reached the maximum limit.", true);
+        }
+    }
+
+    $scope.DeleteFile = function (fileDetail) {
+        fileDetail.IsDeleted = true;
+    }
+
 }])
 
 .controller('toolkitController', ['$scope', function ($scope) {
