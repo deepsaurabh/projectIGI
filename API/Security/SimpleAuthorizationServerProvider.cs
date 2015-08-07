a8cb14f65dc50835a4d48376e4bf8cffe842c992
Ashish Kumar <ashishkumar.abes@gmail.com>
2015-08-07 10:02:19 +0530

    added the role and user name parameter in the token call

--- API/Security/SimpleAuthorizationServerProvider.cs
+++ API/Security/SimpleAuthorizationServerProvider.cs
@@ -1,5 +1,6 @@
 ﻿using Enterprise.Repository;
 using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.OAuth;
 using System;
 using System.Collections.Generic;
@@ -24,7 +25,7 @@ namespace API.Security
         //application/x-www-form-urlencoded
         public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
         {
-
+            IdentityUser user = null;
             using (AuthRepository _repo = new AuthRepository())
             {
                 var username = context.UserName;
@@ -36,25 +37,50 @@ namespace API.Security
                         username = userForEmail.UserName;
                     }
                 }
+                user = await _repo.FindUser(username, context.Password);
 
-                IdentityUser user = await _repo.FindUser(username, context.Password);
                 if (user == null)
                 {
                     context.SetError("invalid_grant", "The username or password is incorrect.");
                     return;
                 }
+
+                var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+                identity.AddClaim(new Claim("user_name", context.UserName));
+
+                var roleCollection = string.Empty;
+                foreach (var role in user.Roles)
+                {
+                    var userRole = _repo.FindRole(role.RoleId);
+                    roleCollection = string.IsNullOrEmpty(roleCollection) ? userRole.Name  : roleCollection + '-' + userRole.Name;
+                    identity.AddClaim(new Claim(ClaimTypes.Role, userRole.Name));
+                }
+
+                AuthenticationProperties properties = CreateProperties(user.UserName, roleCollection);
+                AuthenticationTicket ticket = new AuthenticationTicket(identity, properties);
+
+                context.Validated(ticket);
             }
-            
-            //context.OwinContext.Response.Headers.Add("Role", new[] { userRole });
+        }
 
-            var identity = new ClaimsIdentity(context.Options.AuthenticationType);
-            //identity.AddClaim(new Claim("user_name", context.UserName));
+        public override Task TokenEndpoint(OAuthTokenEndpointContext context)
+        {
+            foreach (KeyValuePair<string, string> property in context.Properties.Dictionary)
+            {
+                context.AdditionalResponseParameters.Add(property.Key, property.Value);
+            }
 
-            //foreach (var role in userRoles)
-            //{
-            //    identity.AddClaim(new Claim(ClaimTypes.Role, role.ToLower()));
-            //}
-            context.Validated(identity);
+            return Task.FromResult<object>(null);
+        }
+
+        public static AuthenticationProperties CreateProperties(string userName, string Roles)
+        {
+            IDictionary<string, string> data = new Dictionary<string, string>
+        {
+            { "userName", userName },
+            {"roles",Roles}
+        };
+            return new AuthenticationProperties(data);
         }
     }
 }
